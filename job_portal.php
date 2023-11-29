33e7e54c91651ee83a99c97b01df567f3153d6be
xli2801 <xli2801@students.cs.ubc.ca>
2023-11-29 14:58:07 -0800

    Updated Projection

--- job_portal.php
+++ job_portal.php
@@ -532,7 +532,7 @@
 
             // Your username is ora_(CWL_ID) and the password is a(student number). For example,
 			// ora_platypus is the username and a12345678 is the password.
-            $db_conn = OCILogon("ora_carolm03", "a17849571", "dbhost.students.cs.ubc.ca:1522/stu");
+            $db_conn = OCILogon("ora_xli2801", "a80002512", "dbhost.students.cs.ubc.ca:1522/stu");
 
             if ($db_conn) {
                 debugAlertMessage("Database is Connected");
